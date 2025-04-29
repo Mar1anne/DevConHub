68f833fb7f2057c933a519ea9cac95d863d43c2a
Mariana Ristovska <mariana.ristovska@gmail.com>
2025-04-29 11:18:59 +0200

    Landing page animation

--- lib/features/home/home_route.dart
+++ lib/features/home/home_route.dart
@@ -1,11 +1,24 @@
 import 'package:devcon_hub/features/home/presentation/view/home_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
 class HomeRoute {
   static const path = '/home';
 
   static GoRoute route = GoRoute(
-    path: path,
-    builder: (context, state) => const HomePage(),
-  );
+      path: path,
+      builder: (context, state) => const HomePage(),
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const HomePage(),
+          transitionDuration: const Duration(milliseconds: 700),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        );
+      });
 }
--- lib/features/landing/presentation/view/landing_page.dart
+++ lib/features/landing/presentation/view/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:video_player/video_player.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -9,16 +10,28 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+  late AnimationController _animationController;
   late Animation<double> _fadeAnimation;
+
+  late VideoPlayerController _videoPlayerController;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    _fadeAnimation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
 
-    _controller.forward();
+    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 4));
+    _fadeAnimation = CurvedAnimation(parent: _animationController, curve: Curves.easeOut);
+
+    _animationController.forward();
+
+    _videoPlayerController =
+        VideoPlayerController.asset(Uri.encodeFull('lib/features/landing/assets/videos/smoke.mp4'))
+          ..setLooping(true)
+          ..setVolume(0.0)
+          ..initialize().then((_) {
+            setState(() {});
+            _videoPlayerController.play();
+          });
 
     Future.delayed(const Duration(seconds: 3), () {
       context.go('/home');
@@ -27,19 +40,44 @@ class _LandingPageState extends State<LandingPage> with SingleTickerProviderStat
 
   @override
   void dispose() {
-    _controller.dispose();
+    _videoPlayerController.dispose();
+    _animationController.dispose();
+
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: FadeTransition(
         opacity: _fadeAnimation,
         child: Center(
-          child: Text(
-            'Welcome to DevCon App',
-            style: Theme.of(context).textTheme.headlineMedium,
+          child: Stack(
+            children: [
+              if (_videoPlayerController.value.isInitialized)
+                SizedBox.expand(
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      width: _videoPlayerController.value.size.width,
+                      height: _videoPlayerController.value.size.height,
+                      child: VideoPlayer(_videoPlayerController),
+                    ),
+                  ),
+                ),
+              Center(
+                child: Text(
+                  'DevCon',
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
