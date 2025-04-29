5291e6654d493724e9d8b3163781fdfdb73fac10
Mariana Ristovska <mariana.ristovska@gmail.com>
2025-04-29 12:12:37 +0200

    Added Speaker model

---
+++ lib/features/speakers/domain/entities/speaker.dart
@@ -0,0 +1,17 @@
+class Speaker {
+  final String id;
+  final String name;
+  final String bio;
+  final String imageUrl;
+  final String company;
+  final String jobTitle;
+
+  const Speaker({
+    required this.id,
+    required this.name,
+    required this.bio,
+    required this.imageUrl,
+    required this.company,
+    required this.jobTitle,
+  });
+}
