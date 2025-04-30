5da02260087fe801041d6898c442ba202231dd2e
Mariana Ristovska <mariana.ristovska@gmail.com>
2025-04-30 14:24:46 +0200

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:devcon_hub/di/injector.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   setupInjector();
   runApp(const MyApp());
 }
