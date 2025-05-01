d5438d40edcd6c6b3661cbd85cbeac78882d626d
Mariana Ristovska <mariana.ristovska@gmail.com>
2025-05-01 15:22:54 +0200

    Added logo to drawer

--- lib/features/home/presentation/view/drawer_menu.dart
+++ lib/features/home/presentation/view/drawer_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
 class DrawerMenu extends StatelessWidget {
@@ -9,32 +10,28 @@ class DrawerMenu extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
+          DrawerHeader(
+            child: Center(
+              child: SvgPicture.asset(
+                'lib/features/home/assets/images/logo_white.svg',
+                colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
+              ),
             ),
-            child: Text('Menu', style: TextStyle(color: Colors.white, fontSize: 24)),
           ),
           ListTile(
             leading: const Icon(Icons.schedule),
             title: const Text('Schedule'),
-            onTap: () {
-              // Navigate to schedule page
-            },
+            onTap: () => context.go('/agenda'),
           ),
           ListTile(
             leading: const Icon(Icons.people),
             title: const Text('Speakers'),
-            onTap: () {
-              context.go('/speakers');
-            },
+            onTap: () => context.go('/speakers'),
           ),
           ListTile(
             leading: const Icon(Icons.link),
             title: const Text('Resources'),
-            onTap: () {
-              // Navigate to resources page
-            },
+            onTap: () => context.go('/resources'),
           ),
         ],
       ),
