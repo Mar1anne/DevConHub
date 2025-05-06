334d0cb13d6b5849beaffa6408c33c139c8f1c83
Mariana Ristovska <mariana.ristovska@gmail.com>
2025-05-06 21:44:50 +0200

    Inject GetIt instance into feature di

--- lib/features/speakers/di/di.dart
+++ lib/features/speakers/di/di.dart
@@ -5,10 +5,8 @@ import 'package:devcon_hub/features/speakers/domain/repositories/speaker_reposit
 import 'package:devcon_hub/features/speakers/domain/usecases/get_speakers.dart';
 import 'package:get_it/get_it.dart';
 
-final injector = GetIt.instance;
-
 class SpeakersInjector {
-  static void initialize() {
+  static void initialize(GetIt injector) {
     injector
       ..registerLazySingleton<FirebaseSpeakerDataSource>(
         () => FirebaseSpeakerDataSource(FirebaseFirestore.instance),
